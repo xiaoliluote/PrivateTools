638863c584078eeae47a3adbc755c62f87e3827a
xiaoliluote <1406681203@qq.com>
2018-08-14 14:49:57 +0800

    提交相机/相册功能

--- example/src/main/java/com/killaxiao/privatetools/MainActivity.java
+++ example/src/main/java/com/killaxiao/privatetools/MainActivity.java
@@ -1,13 +1,20 @@
 package com.killaxiao.privatetools;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.dfqin.grantor.PermissionListener;
+import com.github.dfqin.grantor.PermissionsUtil;
+
+import me.nereo.multi_image_selector.MultiImageSelector;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button bt_httprequest;
+    private Button bt_httprequest,bt_photo_album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView(){
         bt_httprequest = (Button)findViewById(R.id.bt_httprequest);
+        bt_photo_album = (Button)findViewById(R.id.bt_photo_album);
     }
 
     private void initListener(){
         bt_httprequest.setOnClickListener(this);
+        bt_photo_album.setOnClickListener(this);
     }
 
     @Override
@@ -33,6 +42,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.bt_httprequest:
                 intent = new Intent(this,HttpRequestExample.class);
                 startActivity(intent);
+                break;
+            case R.id.bt_photo_album:
+                PermissionsUtil.requestPermission(this, new PermissionListener() {
+
+                    @Override
+                    public void permissionGranted(@NonNull String[] permissions) {
+
+                        MultiImageSelector selector = MultiImageSelector.create();
+
+                        selector.showCamera(true);   //是否显示拍照功能
+
+                        selector.count(3);   //允许选择几张图片
+                        selector.multi();   //是否多张模式， 如果是单张模式，则方法为single();
+                        selector.start(MainActivity.this, 0x1);   //自定义一个int参数返回至onActivityResult
+                    }
+
+
+                    @Override
+                    public void permissionDenied(@NonNull String[] permissions) {
+
+                    }
+                }, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE});
                 break;
         }
     }
